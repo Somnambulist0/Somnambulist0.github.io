523bec58120ce3b4b18bca0656a90c658c655380
Somnambulist0 <95619097+Somnambulist0@users.noreply.github.com>
2023-09-10 22:09:20 -0400

    version0

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const nameIntro = document.querySelector('.name-intro');
-    const sections = document.querySelectorAll('.section');
+    const fullNameDiv = document.querySelector('.full-name');
 
-    // 初始动画
-    setTimeout(() => {
-        nameIntro.style.opacity = "1";
-        nameIntro.style.transform = "translateY(0px)";
-    }, 500);
-
-    setTimeout(() => {
-        sections.forEach((section, index) => {
-            setTimeout(() => {
-                section.style.opacity = "1";
-                section.style.transform = "translateY(0px)";
-            }, 500 * (index + 1));
+    fullNameDiv.addEventListener('click', function () {
+        window.scrollTo({
+            top: window.innerHeight,
+            behavior: 'smooth'
         });
-    }, 1500);
+    });
 });
